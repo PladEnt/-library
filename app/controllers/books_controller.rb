af1bce3da38ac7d2144bbf47f3e882d3238b8922
PladEnt <joshuadforrest@gmail.com>
2019-05-08 12:50:30 -0700

    stuff

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,69 @@
+class BooksController < ApplicationController
+    def index
+        if params[:auther]
+          @auther = Auther.find_by(id: params[:auther])
+          if @auther.nil?
+            redirect_to authers_path, alert: "auther not found"
+          else
+            @books = @auther.books
+          end
+        else
+          @books = book.all
+        end
+      end
+    
+      def show
+        if params[:auther]
+          @auther = Auther.find_by(id: params[:auther])
+          @book = @auther.books.find_by(id: params[:id])
+          if @book.nil?
+            redirect_to auther_books_path(@auther), alert: "book not found"
+          end
+        else
+          @book = Book.find(params[:id])
+        end
+      end
+    
+      def new
+        @book = Book.new
+      end
+    
+      def create
+        @book = Book.new(book_params)
+    
+        if @book.save
+          redirect_to @book
+        else
+          render :new
+        end
+      end
+    
+      def edit
+        @book = Book.find(params[:id])
+      end
+    
+      def update
+        @book = Book.find(params[:id])
+    
+        @book.update(book_params)
+    
+        if @book.save
+          redirect_to @book
+        else
+          render :edit
+        end
+      end
+    
+      def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        flash[:notice] = "book deleted."
+        redirect_to books_path
+      end
+    
+      private
+    
+      def book_params
+        params.require(:book).permit(:title, :auther_name)
+      end
+end
