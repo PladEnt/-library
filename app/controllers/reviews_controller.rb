1b9c36606db965b03d9c881836df6edab651847f
PladEnt <joshuadforrest@gmail.com>
2019-05-09 07:17:57 -0700

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,30 @@
 class ReviewsController < ApplicationController
+  def show
+      @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
     
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "user deleted."
+    redirect_to users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :books, only: [:index, :show]
+  end
+  resources :books
+
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
 end
