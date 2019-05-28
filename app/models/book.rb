d2e47196542676ceff74fbc1b7c0c86addb8ddd6
PladEnt <joshuadforrest@gmail.com>
2019-05-28 12:56:10 -0700

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
     has_many  :reviews
+    has_many :reviewed_users, through: :reviews, source: :user
+    belongs_to :user
+
+    validates_presence_of :title
+    validates_presence_of :auther
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_many  :reviews
+    has_many :reviewed_books, through: :reviews, source: :book
+    has_many :books
+
+    validates :name, uniqueness: true
+    has_secure_password
 end
