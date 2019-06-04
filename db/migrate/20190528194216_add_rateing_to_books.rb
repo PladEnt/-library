61371add0690a547ee9542c918c4d815d7df8a68
PladEnt <joshuadforrest@gmail.com>
2019-06-04 07:08:51 -0700

    migrations

--- db/migrate/20190528194216_add_rateing_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddRateingToBooks < ActiveRecord::Migration[5.2]
-  def change
-    add_column(:books, :rateing, :integer)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_194216) do
+ActiveRecord::Schema.define(version: 2019_06_04_134744) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_194216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "rateing"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
@@ -28,12 +27,14 @@ ActiveRecord::Schema.define(version: 2019_05_28_194216) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.string "password_digest"
   end
 
